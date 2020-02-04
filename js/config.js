32d6e096044e5bdc11a46d338852ece45929666a
Scott McAllister <smcallister@pagerduty.com>
2020-02-04 15:05:25 -0800

    first push to PD org

---
+++ js/config.js
@@ -0,0 +1,4 @@
+const APP_CONFIG = {
+    redirectUrl: `https://pagerduty.github.io/pagerduty-service-maintenance-sample/`,
+    clientId: `164062530f9e975cd1973364efdd5c50234ac303417584d2502092d163a2f783`
+};
