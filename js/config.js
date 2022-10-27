6612f15fcb9e31403670b198925ce8cf83c96b83
Scott McAllister <smcallister@pagerduty.com>
2022-10-27 14:07:27 -0700

    update config

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const APP_CONFIG = {
     redirectUrl: `https://pagerduty.github.io/pagerduty-service-maintenance-sample/`,
-    clientId: `164062530f9e975cd1973364efdd5c50234ac303417584d2502092d163a2f783`
+    clientId: `cb2ba0d5-2bc0-4605-b728-ee4dfb693baf`
 };
